a07db8ecafc8ee4da6e7d8f5b24e7af0661cf0dd
Richmond Ampaabeng <richmondampaabeng@gmail.com>
2022-08-15 17:19:06 +0100

    Add, deletion off console.log(form)

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@
 // TODO: Select the form element form the HTML
 const form = document.querySelector("form"); // document is a reference to the page
 // DOM: document object module
-console.log(form); // console
+// console
