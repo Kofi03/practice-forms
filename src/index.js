82b4077126621bb3cba617bc217ec8e8cb7d2bb6
Richmond Ampaabeng <richmondampaabeng@gmail.com>
2022-08-15 17:18:41 +0100

    Select the form element form the HTML

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-console.log("hello");
+// TODO : log the from submission
+// TODO: Select the form element form the HTML
+const form = document.querySelector("form"); // document is a reference to the page
+// DOM: document object module
+console.log(form); // console
