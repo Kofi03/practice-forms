7dee172520fc4da60ea012fbebe01353ee749a03
Richmond Ampaabeng <richmondampaabeng@gmail.com>
2022-08-15 18:10:22 +0100

    Add localstorage

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@
 const form = document.querySelector("form"); // document is a reference to the page
 // DOM: document object module
 // console
+console.log(localStorage.getItem("username"));
+
 form.addEventListener("submit", (itHappened) => {
   itHappened.preventDefault();
-  const submission = Object.fromEntries(new FormData(event.target));
+  const submission = Object.fromEntries(new FormData(itHappened.target));
   console.log(submission);
+
+  if (Object.keys(submission).includes("saveUsername")) {
+    localStorage.setItem("username", submission.username);
+  }
 });
